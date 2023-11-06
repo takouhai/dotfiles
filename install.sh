79493ed72e993f4b3fb6f0b9b00555e1b7a3b91a
Daniel Alejandro Gallegos <daniel@tacowolf.net>
2023-11-06 12:54:27 -0500

    fix: update install script to suck less

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 echo "removing old home manager configs"
-rm ~/.config/home-manager/*
-rm ~/.config/nix/nix.conf
+rm -fr ~/.config/home-manager
+rm -fr ~/.config/nix/nix.conf
 
 echo "creating a copy of the dotfiles into the config"
-ln flake.nix ~/.config/home-manager
-ln flake.lock ~/.config/home-manager
-ln home.nix ~/.config/home-manager
+mkdir ~/.config/home-manager/
+ln flake.nix ~/.config/home-manager/
+ln flake.lock ~/.config/home-manager/
+ln home.nix ~/.config/home-manager/
+mkdir ~/.config/nix/
 ln nix/nix.conf ~/.config/nix/nix.conf
